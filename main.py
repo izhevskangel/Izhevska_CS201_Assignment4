a2c2854599edf3a3c94b5428507221bd94969c18
a.izhevska <a.izhevska@gmail.com>
2026-04-19 21:54:17 +0300

    відфільтровано в файл син ісусв

--- main.py
+++ main.py
@@ -3,4 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("random_walk.csv")
-print(df.head())
+df["distance"] = np.sqrt(df["x"]**2 + df["y"]**2)
+print(df["distance"].max())
+print(df["distance"].mean())
+# print(df.head())
+
+filtered = df[df["distance"]>df["distance"].mean()]
+print(filtered.head())
+filtered.to_json("filtered.json", orient="records", indent = 2)
