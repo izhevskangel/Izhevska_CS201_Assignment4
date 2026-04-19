1dc228c19054683f86b5707605b8f7391aeb6ed4
a.izhevska <a.izhevska@gmail.com>
2026-04-19 22:19:40 +0300

    додано графік

--- main.py
+++ main.py
@@ -11,3 +11,15 @@ print(df["distance"].mean())
 filtered = df[df["distance"]>df["distance"].mean()]
 print(filtered.head())
 filtered.to_json("filtered.json", orient="records", indent = 2)
+
+plt.figure(figsize=(9, 7))
+plt.plot(df["x"], df["y"], color = "pink", linewidth = 2,
+         linestyle = "-", alpha = 0.67, label = "траєкторія")
+plt.scatter(df["x"][:1], df["y"][:1], color = "deeppink", s = 100, label = "початок")
+plt.scatter(df["x"][-1:], df["y"][-1:], color = "deeppink", s = 100, marker = "x", label = "кінець")
+plt.title("Траєкторія випадкового блукання")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.legend()
+plt.grid(True)
+plt.show()
